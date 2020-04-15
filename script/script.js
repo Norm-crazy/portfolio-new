e2b85b593c458e241aa820f4c152244c660456dc
Norman Higgs <normcrazy@yahoo.com>
2020-04-15 08:42:03 -0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,6 +1,52 @@
 $(document).ready(function() {
 
-  
+  var infoArea = $(".infoBox");
+
+  var buttons = $("button");
+
+  $(buttons).click(function() {
+    $("#btnInfo").hide('-100px');
+  })
+
+  function removeInfo() {
+    if ($(infoArea).is(':visible')) {
+      $(infoArea).hide();
+    }
+  }
+
+  function btnColor() {
+    $(buttons).removeAttr('id');
+  }
+
+  $("#about").click(function() {
+    if ($("#aboutText").is(':visible')) {
+        $(this).attr('id', 'btnSelect');
+    }
+    else {
+      removeInfo();
+      $("#aboutText").show({
+        'left': '-700px'
+      });
+    }
+  });
+
+  $("#exp").click(function() {
+    btnColor();
+    removeInfo();
+    $("#xpText").show({
+      'left': '-700px'
+    });
+  });
+
+  $("#pro").click(function() {
+    if ($(infoArea).is(':visible')) {
+      console.log("seen");
+    } else {
+      console.log("hide");
+    }
+  });
+
+
 
 
   // End of document.redy
